33e105f7888716de80cef80dd14d11292c3c40fe
beom-jin.kim <beom-jin.kim@samsung.com>
2025-07-23 16:00:55 +0900

    [feature] Step 5 - define max and min lenght variables

--- SimilarityChecker/similarity-checker.cpp
+++ SimilarityChecker/similarity-checker.cpp
@@ -17,12 +17,14 @@ public:
 			if (each.length() == 0) throw length_error("At least one letter");
 		}
 
-		if (input[0].length() == input[1].length()) return 60;
-		if (input[0].length() < input[1].length()) swap(input[0], input[1]);
-		if (input[1].length() * 2 <= input[0].length()) return 0;
+		int maxLength = max(input[0].length(), input[1].length());
+		int minLength = min(input[0].length(), input[1].length());
 
-		int diff = input[0].length() - input[1].length();
-		return 60 - 60 * diff / input[1].length();
+		if (maxLength == minLength) return 60;
+		if (minLength * 2 <= maxLength) return 0;
+
+		int diff = maxLength - minLength;
+		return 60 - 60 * diff / minLength;
 	}
 
 	void AddInputStrings(vector<string>& input) {
